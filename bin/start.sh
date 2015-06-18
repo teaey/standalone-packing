d44ce8c1d9eaadedf285fdac68981ac218a44910
teaey <masfay@163.com>
2015-06-18 17:05:19 +0800

    complete start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,19 @@
-
+#!/bin/bash
 BIN_PATH=$(cd "$(dirname "$0")"; pwd)
 cd $BIN_PATH
 cd ..
 SOFT_HOME=$(pwd)
 
-java -classpath $SOFT_HOME/libs/**.jar Main
+CLASSPATH=
+for i in `ls $SOFT_HOME/libs/*.jar`
+do
+    CLASSPATH=$CLASSPATH:$i
+done
+
+CLASSPATH=${CLASSPATH#*:}
+
+echo $CLASSPATH
+
+java -classpath $CLASSPATH Main
 
 read -n 1 -p "Press any key to continue..."
-
